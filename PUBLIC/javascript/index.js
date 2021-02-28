d7e577d8fb2bb780c3a567aca6a21fce0346a5aa
ces <franceschicecile@gmail.com>
2021-02-28 09:17:26 +0100

    javascript antoine

--- PUBLIC/javascript/index.js
+++ PUBLIC/javascript/index.js
@@ -1,1 +1,17 @@
-const Teddy = require('../models/Teddy');
+function afficherProduits(){
+  console.log("test");
+}
+
+afficherProduits();
+
+fetch("http://localhost:3000/api/teddies")
+.then(response => response.json())
+.then(response => alert(JSON.stringify(response)))
+.catch(error => alert("Erreur : " + error));
+
+function afficherProduits(){
+  document.getElementsByClassName("row");
+  document.getElementById('teddies').style.height = 'auto';
+}
+
+
