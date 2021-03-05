6f09a8ab862ea3d4ca0dcc5d1bf8879861040591
ces <franceschicecile@gmail.com>
2021-03-05 15:00:52 +0100

    recup données

--- PUBLIC/javascript/index.js
+++ PUBLIC/javascript/index.js
@@ -9,6 +9,21 @@ fetch("http://localhost:3000/api/teddies")
 .then(response => alert(JSON.stringify(response)))
 .catch(error => alert("Erreur : " + error));
 
+const promise1 = fetch("http://localhost:3000/api/teddies");
+promise1
+.then((response) =>{
+  console.log(response);
+  const teddiesData = response.json();
+  console.log(teddiesData);
+
+  teddiesData.then((utilisateur) => {
+  console.log(utilisateur)
+  });
+})
+
+
+
+
 function async (listeProduits) {
   const articles = await (getArticles)  
   displayArticles(articles)
@@ -31,5 +46,5 @@ function displayArticles(){
 }
 
 let teddies = document.getElementById("teddies");
-alert(teddies);
+alert(teddies.innerHTML);
 console.log(teddies);
