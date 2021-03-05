790f6241590c2919d3b83459692dd7fe054fc168
ces <franceschicecile@gmail.com>
2021-03-05 09:24:52 +0100

    essai js

--- PUBLIC/javascript/index.js
+++ PUBLIC/javascript/index.js
@@ -1,17 +1,35 @@
-function afficherProduits(){
+function listeProduits(){
   console.log("test");
 }
 
-afficherProduits();
+listeProduits();
 
 fetch("http://localhost:3000/api/teddies")
 .then(response => response.json())
 .then(response => alert(JSON.stringify(response)))
 .catch(error => alert("Erreur : " + error));
 
-function afficherProduits(){
-  document.getElementsByClassName("row");
-  document.getElementById('teddies').style.height = 'auto';
+function async (listeProduits) {
+  const articles = await (getArticles)  
+  displayArticles(articles)
+  console.log (articles)
 }
 
 
+function getArticles(){
+return fetch("http://localhost:3000/api/teddies")
+.then(function(httpBodyResponse){  
+  return httpBodyResponse.json()
+})
+.then(function(articles){
+console.log(articles)
+
+})
+}
+function displayArticles(){
+  document.getElementsById("listeProduits")
+}
+
+let teddies = document.getElementById("teddies");
+alert(teddies);
+console.log(teddies);
