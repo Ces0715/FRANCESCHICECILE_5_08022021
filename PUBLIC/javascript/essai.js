79149673428f0a3f9d5390e28ebb625e60a196b6
ces <franceschicecile@gmail.com>
2021-03-17 10:11:59 +0100

    recup article api

--- PUBLIC/javascript/essai.js
+++ PUBLIC/javascript/essai.js
@@ -9,9 +9,6 @@ produits.then((response) =>{
 })
 
 
-
-
-
 /*Norbert*/
   teddiesData.then((utilisateur) => {
     console.log(utilisateur[0]);
@@ -130,13 +127,7 @@ console.log(affichageId);
     affichageName.innerHTML =txtName1;
     affichageName.innerHTML =txtName2;
     affichageName.innerHTML =txtName3;
-    affichageName.innerHTML =txtName4;
-
-
-    
-
-
-    
+    affichageName.innerHTML =txtName4;  
   });
   })
 
@@ -216,3 +207,179 @@ for(let i = 0 ; i < AllTeddies.length  ; i++ ){
   let prix = AllTeddies[i].price;
 	
 }
+
+let tabTeddies = [teddy1, teddy2, teddy3, teddy4, teddy5];
+
+
+
+for (let i = 0; i < tabTeddies.length; i++) {
+  console.log(tabTeddies[i]);
+  let nom = tabTeddies[i].name;
+  console.log(nom);
+  let description = tabTeddies[i].description;
+  console.log(description);
+  teddies.innerHTML +=`<div class="row mb-3"><div class="col-sm-6 col-lg-6 themed-grid-col">      
+   <div class=" col-sm-6 col-lg-6 themed-grid-col"> <h2 id="name">${tabTeddies[i].name } <p id="description">
+   - ${tabTeddies[i].description }</div class>`;
+  }
+
+
+
+  try{
+    /*recup data API*/
+    let name = responseData.name;
+    let _id = response._id;
+    let imageUrl = response.imageUrl;
+    let description = response.description;
+    console.log(name);
+    console.log(_id);
+    console.log(imageUrl);
+    console.log(description);
+  
+    for (let i = 0; i < name.length; i++){ 
+      console.log(name[i].innerHTML) 
+  }
+  
+    //affichage
+    let affichage_name = document.querySelector("#name");
+    affichage_name.innerHTML = name;
+    
+    let affichage_imageUrl = document.querySelector("#imageUrl");
+    affichage_imageUrl.innerHTML = imageUrl;
+    let affichage_description = document.querySelector("#description");
+    affichage_description.innerHTML = description;
+  
+  
+    for (let i = 0; i < name.length; i++){ 
+      console.log(name[i].innerHTML) ;
+     
+  }
+  
+  } catch (err){
+    console.log(err);
+
+
+
+
+    /*Antoine info*/
+function listeProduits(){
+  console.log("test");
+}
+listeProduits();
+fetch("http://localhost:3000/api/teddies")
+.then(response => response.json())
+.then(response => alert(JSON.stringify(response)))
+.catch(error => alert("Erreur : " + error));
+
+
+
+/*recuperation données*/
+const produits = fetch("http://localhost:3000/api/teddies");
+produits.then (async (responseData) =>{
+  console.log(responseData);
+
+const articles =await responseData.json();
+for(let i = 0; i < articles.length; i++){
+  const article = articles[i];
+  displayArticle(article)
+}
+console.log(articles);  
+//données ok
+
+
+function displayArticle(){
+  document.getElementById("teddies").innerHTML +='<article class="teddy"><span id="name"></span><span id="_id"></span><span id="imageUrl"></span><span id="description"></span></article>'
+}
+
+
+
+/*declaration des teddies*/
+let teddiesData = ['Norbert', 'Arold', 'Lenny et Carl', 'Gustav', 'Garfunkel'];
+console.log(teddiesData);
+
+/* lien avec html*/
+let teddies = document.querySelector('#teddies');
+console.log(teddies);
+
+/*creation boucle*/
+for (let i = 0; i < teddiesData.length; i++) {
+  console.log(`Teddy ${teddiesData[i]}`);
+  
+  //console.log(teddiesData[i])
+  
+  
+ // teddies.innerHTML += '<div class="row mb-3"><div class="col-sm-6 col-lg-6 themed-grid-col">       <div class=" col-sm-6 col-lg-6 themed-grid-col">' +teddiesData[i]+ '</div class>';
+
+}
+
+
+let teddy1 = {
+  'name' : 'Norbert',
+  'id' : '5be9c8541c9d440000665243',
+  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'imageURL':'http://localhost:3000/images/teddy_1.jpg',
+  'price':'2900',
+  'colors':'"Tan", "Chocolate", "Black", "White"',
+};
+
+
+
+
+let teddy2 = {
+  'name' : 'Arold',
+  'id' : '5beaa8bf1c9d440000a57d94',
+  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'imageURL':'http://localhost:3000/images/teddy_2.jpg',
+  'price':'3900',
+  'colors':'"Pale brown", "Dark brown", "White"',
+}
+
+
+let teddy3 = {
+  'name' : 'Lenny and Carl',
+  'id' : '5beaaa8f1c9d440000a57d95',
+  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'imageURL':'http://localhost:3000/images/teddy_3.jpg',
+  'price':'5900',
+  'colors':'" Brown"',
+}
+
+
+let teddy4 = {
+  'name' : 'Gustav',
+  'id' : '5beaabe91c9d440000a57d96',
+  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'imageURL':'http://localhost:3000/images/teddy_4.jpg',
+  'price':'4500',
+  'colors':'" Brown", "Blue", "Pink"',
+}
+
+
+let teddy5 = {
+  'name' : 'Garfunkel',
+  'id' : '5beaacd41c9d440000a57d97',
+  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'imageURL':'http://localhost:3000/images/teddy_5.jpg',
+  'price':'5500',
+  'colors':'" Beige", "Tan", "Chocolate"',
+}
+
+
+})
+
+
+
+/*recuperation données*/
+const produits = fetch("http://localhost:3000/api/teddies");
+produits.then (async (responseData) =>{
+  console.log(responseData);
+
+const articles =await responseData.json();
+for(let i = 0; i < articles.length; i++){
+  const article = articles[i];
+  console.log(article);
+}
+console.log(articles);  
+//données ok
+
+})
--- PUBLIC/javascript/index.js
+++ PUBLIC/javascript/index.js
@@ -9,117 +9,28 @@ fetch("http://localhost:3000/api/teddies")
 .then(response => alert(JSON.stringify(response)))
 .catch(error => alert("Erreur : " + error));
 
-
+function displayArticle(){
+  document.getElementById("teddies").innerHTML +='<article class="teddy"><span id="name"></span><span id="_id"></span><span id="imageUrl"></span><span id="description"></span></article>'
+}
 
 /*recuperation données*/
 const produits = fetch("http://localhost:3000/api/teddies");
-
 produits.then (async (responseData) =>{
   console.log(responseData);
-const response =await responseData.json();
-console.log(response[0]);  
 
-try{
-  /*recup data API*/
-  const name = response[0].name;
-  const _id = response[0].id;
-  const imageUrl = response[0].imageUrl;
-  const description = response[0].description;
-  console.log(name);
-  console.log(_id);
-  console.log(imageUrl);
-  console.log(description);
+const articles =await responseData.json();
+for(let i = 0; i < articles.length; i++){
+  const article = articles[i];
+  displayArticle(article)
+  console.log(article); 
+  console.log(articles);  
+//données ok
 }
-catch(err){
-  console.log(err);
+
+
+function displayArticle(article){
+document.getElementById("teddies").innerHTML +=`<div class="row mb-3"><div class="col-sm-6 col-lg-6 themed-grid-col">      
+<div class=" col-sm-6 col-lg-6 themed-grid-col"> <h2 id="name">${article.name } <p id="description">
+- ${article.description }</div class>`;
 }
 })
-
-
-/*declaration des teddies*/
-let teddiesData = ['Norbert', 'Arold', 'Lenny et Carl', 'Gustav', 'Garfunkel'];
-console.log(teddiesData);
-
-/* lien avec html*/
-let teddies = document.querySelector('#teddies');
-console.log(teddies);
-
-/*creation boucle*/
-for (let i = 0; i < teddiesData.length; i++) {
-  console.log(`Teddy ${teddiesData[i]}`);
-  
-  //console.log(teddiesData[i])
-  
-  
- // teddies.innerHTML += '<div class="row mb-3"><div class="col-sm-6 col-lg-6 themed-grid-col">       <div class=" col-sm-6 col-lg-6 themed-grid-col">' +teddiesData[i]+ '</div class>';
-
-}
-
-
-let teddy1 = {
-  'name' : 'Norbert',
-  'id' : '5be9c8541c9d440000665243',
-  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'imageURL':'http://localhost:3000/images/teddy_1.jpg',
-  'price':'2900',
-  'colors':'"Tan", "Chocolate", "Black", "White"',
-};
-
-
-
-
-let teddy2 = {
-  'name' : 'Arold',
-  'id' : '5beaa8bf1c9d440000a57d94',
-  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'imageURL':'http://localhost:3000/images/teddy_2.jpg',
-  'price':'3900',
-  'colors':'"Pale brown", "Dark brown", "White"',
-}
-
-
-let teddy3 = {
-  'name' : 'Lenny and Carl',
-  'id' : '5beaaa8f1c9d440000a57d95',
-  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'imageURL':'http://localhost:3000/images/teddy_3.jpg',
-  'price':'5900',
-  'colors':'" Brown"',
-}
-
-
-let teddy4 = {
-  'name' : 'Gustav',
-  'id' : '5beaabe91c9d440000a57d96',
-  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'imageURL':'http://localhost:3000/images/teddy_4.jpg',
-  'price':'4500',
-  'colors':'" Brown", "Blue", "Pink"',
-}
-
-
-let teddy5 = {
-  'name' : 'Garfunkel',
-  'id' : '5beaacd41c9d440000a57d97',
-  'description':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  'imageURL':'http://localhost:3000/images/teddy_5.jpg',
-  'price':'5500',
-  'colors':'" Beige", "Tan", "Chocolate"',
-}
-
-
-
-let tabTeddies = [teddy1, teddy2, teddy3, teddy4, teddy5];
-
-
-
-for (let i = 0; i < tabTeddies.length; i++) {
-  console.log(tabTeddies[i]);
-  let nom = tabTeddies[i].name;
-  console.log(nom);
-  let description = tabTeddies[i].description;
-  console.log(description);
-  teddies.innerHTML +=`<div class="row mb-3"><div class="col-sm-6 col-lg-6 themed-grid-col">      
-   <div class=" col-sm-6 col-lg-6 themed-grid-col"> <h2 id="name">${tabTeddies[i].name } <p id="description">
-   - ${tabTeddies[i].description }</div class>`;
-  }
