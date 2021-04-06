35a08be486027a7ceee62fa133e10c31d45ca675
ces <franceschicecile@gmail.com>
2021-04-06 15:44:12 +0200

    modif antoine 6 avril

--- PUBLIC/javascript/product.js
+++ PUBLIC/javascript/product.js
@@ -14,72 +14,15 @@ const response = fetch (`http://localhost:3000/api/teddies/${id}`);
 response.then(async(data)=>{
 
     const article = await data.json();
-   //for (const propt in article){
-     //console.log(article[propt]); 
-     //console.log(article.colors) ;  
-
-   //for (const couleur of article.colors){
-     //console.log(couleur);}
-   //boucle couleurs
-   //let optionCouleur = [""];
-   //article.colors.forEach((couleur) =>{
-    //console.log(couleur);
+   
     
 //preparation structure HTML pour affichage produit
-  document.getElementById("produit").innerHTML += 
-`<div class="row mb-3">     
-    <div class=" col-sm-6 col-lg-6 themed-grid-col"> 
-      <h2 id="name">${article.name}</h2> 
-      <p id="id">Reférence:${article._id}</p>
-      <p id="price">Prix: ${article.price/100} €</p> 
+ afficherProduit(article);
 
-      <form>
-        <label id ="labelOptions"> Couleur:
-          <select name="choix" id="choix"> </select>
-        </label> 
-      </form>
-
-      <form>
-        <label id="selectQuantity"> Quantité :
-          <select name ="quantity" id ="choose"></select> 
-        </label>
-      </form>
-    </div>
-    <div class="col-sm-6 col-lg-6 themed-grid-col"> 
-      <a href="./panier.html?id=${'_id'}">
-      <img width="300"  src = "${article.imageUrl}">
-      <button id ="btn-envoyer" type="submit name="btn-envoyer">Ajouter au panier</button>
-    </div> `;
-
-//adapter le formulaire au nombre d'options couleurs du produit
-const optionCouleur = article.colors;
-//ou utiliser const optionCouleur = article["colors"];
-let structureOptions = [];
-// boucle for pour afficher options couleur
-for(let j = 0; j < optionCouleur.length; j++){
-  structureOptions = structureOptions + 
- `<option value = "${j}">${optionCouleur[j]}  </option> `;         
-}
-//console.log(structureOptions);
 const choixCouleur = document.querySelector("#choix");
-choixCouleur.innerHTML = structureOptions;
-
-// choisir nombre
-let optionNombre = ['1', '2', '3', '4', '5'];
-//console.log(optionNombre);
-
-let structureNombre =[];
-//console.log(structureNombre);
-for (let k = 0; k < optionNombre.length; k++){
-  structureNombre = structureNombre +
-  `<option value = "${k}">${optionNombre[k]}  </option> `;         
-}
-//console.log(structureNombre);
-const choixNombre = document.querySelector("#choose");
-choixNombre.innerHTML = structureNombre;
-
 const choixForm = choixCouleur.value;
 console.log(choixForm);
+const choixNombre = document.querySelector("#choose");
 const choixForm2 = choixNombre.value;
 console.log(choixForm2);
 
@@ -140,3 +83,50 @@ else{
 }) 
 });
 
+function afficherProduit(article) {
+  document.getElementById("produit").innerHTML += 
+  `<div class="row mb-3">     
+      <div class=" col-sm-6 col-lg-6 themed-grid-col"> 
+        <h2 id="name">${article.name}</h2> 
+        <p id="id">Reférence:${article._id}</p>
+        <p id="price">Prix: ${article.price/100} €</p> 
+  
+        <form>
+          <label id ="labelOptions"> Couleur:
+            <select name="choix" id="choix"> </select>
+          </label> 
+        </form>
+  
+        <form>
+          <label id="selectQuantity"> Quantité :
+            <select name ="quantity" id ="choose"></select> 
+          </label>
+        </form>
+      </div>
+      <div class="col-sm-6 col-lg-6 themed-grid-col"> 
+        <a href="./panier.html?id=${'_id'}">
+        <img width="300"  src = "${article.imageUrl}">
+        <button id ="btn-envoyer" type="submit name="btn-envoyer">Ajouter au panier</button>
+      </div> `;
+  
+  //les options couleurs du produit
+  const optionCouleur = article.colors;
+  let structureOptions = "";
+  // boucle for pour afficher options couleur
+  for(let j = 0; j < optionCouleur.length; j++){
+    structureOptions +=  `<option value = "${j}"> ${optionCouleur[j]}  </option> `;         
+  }
+  const choixCouleur = document.querySelector("#choix");
+  choixCouleur.innerHTML = structureOptions;
+  
+  
+  // choisir nombre
+  let optionNombre = ['1', '2', '3', '4', '5'];
+  let structureNombre =[];
+  for (let k = 0; k < optionNombre.length; k++){
+    structureNombre += `<option value = "${k}">${optionNombre[k]}  </option> `;         
+  }
+  //console.log(structureNombre);
+  const choixNombre = document.querySelector("#choose");
+  choixNombre.innerHTML = structureNombre;
+}
