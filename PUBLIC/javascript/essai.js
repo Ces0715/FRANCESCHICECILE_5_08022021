b00f979de9b54cbdd24143b3e27349ac4a28adc7
ces <franceschicecile@gmail.com>
2021-05-07 17:44:35 +0200

    modif a refaire

--- PUBLIC/javascript/essai.js
+++ PUBLIC/javascript/essai.js
@@ -398,3 +398,318 @@ console.log(articles);
 //function displayArticle(){
   //document.getElementById("teddies").innerHTML +='<article class="teddy"><span id="name"></span><span id="_id"></span><span id="imageUrl"></span><span id="description"></span></article>'
 //}
+
+
+
+//******************************************************************************************** */
+//CREATION CLASSE POUR CREER UN OBJET CONTENANT LES VALEURS DU FORMULAIRE
+class Formulaire {
+  constructor() {
+      this.nom = document.querySelector("#nom").value;
+      this.prenom = document.querySelector("#prenom").value;
+      this.mail = document.querySelector("#mail").value;
+      this.numero = document.querySelector("#numero").value;
+      this.adresse = document.querySelector("#adresse").value;
+      this.codepostal = document.querySelector("#codepostal").value;
+      this.ville = document.querySelector("#ville").value;
+  }
+}
+// appel de l 'instance de classe Formulaire pour creer l'objet formulairevalues(recup des valeurs)
+const formulaireValues = new Formulaire();
+
+
+//**************************************GESTION VALIDER FORMULAIRE****************************************************/
+
+const alerter = (value) => {
+  return `${value}:chiffres non autorisés \n caractères compris entre 3 et 20`;
+}
+const regExNomPrenomVille = (value) => {
+  return /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(value);
+}
+const regExCodePostal = (value) => {
+  return /^[0-9]{5}$/.test(value);
+}
+const regExEmail = (value) => {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
+}
+
+const regExAdresse = (value) => {
+  return /^[A-Za-z0-9\s]{5,50}$/.test(value);
+}
+
+function controlePrenom() {
+  //controle du prenom
+  const lePrenom = formulaireValues.prenom;
+  if (regExNomPrenomVille(lePrenom)) {
+      console.log("ok");
+      return true;
+  } else {
+      console.log("ko");
+      alert(alerter("Prenom"));
+      return false;
+  }
+};
+
+function controleNom() {
+  //controle du nom
+  const leNom = formulaireValues.nom;
+  if (regExNomPrenomVille(leNom)) {
+      console.log("ok");
+      return true;
+  } else {
+     console.log("ko");
+      alert(alerter("Nom"));
+      return false;
+  }
+};
+
+function controleCodePostal() {
+  //controle du code postal
+  const leCodePostal = formulaireValues.codepostal;
+  if (regExCodePostal(leCodePostal)) {
+      console.log("ok");
+      return true;
+  } else {
+      console.log("ko");
+     alert("Le code postal doit etre composé de 5 chiffres");
+      return false;
+  }
+};
+
+function controleEmail() {
+  //controle de mail
+  const leEmail = formulaireValues.mail;
+  if (regExEmail(leEmail)) {
+      console.log("ok");
+      return true;
+  } else {
+      console.log("ko");
+     alert(" l'Email n'est pas correct");
+      return false;
+  }
+};
+
+function controleAdresse() {
+  //controle de l'adresse
+  const leAdresse = formulaireValues.adresse;
+  if (regExAdresse(leAdresse)) {
+      console.log("ok");
+      return true;
+  } else {
+      console.log("ko");
+     alert(" l'adresse n'est pas correcte");
+      return false;
+  }
+};
+  
+  //controle validité du formulaire avant envoi dans le LS
+
+  if (controlePrenom() && controleNom() && controleCodePostal() && controleEmail() && controleAdresse()) {
+      //******mettre l'objet formulaireValues dans le localstorage****** */
+      localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
+      //window.location.href = "confirmation.html";
+  } else {
+      alert("Veuillez bien remplir le formulaire");
+  }
+
+  //**************************************GESTION VALIDER FORMULAIRE****************************************************/
+
+  // creation objet du formulaire et produits a mettre dans un objet pour l'envoyer au serveur
+  const aEnvoyer = {
+      produitLocal,
+      formulaireValues,
+  };
+  console.log(aEnvoyer);
+
+
+  const url = 'http://localhost:3000/api/teddies/order';
+  console.log(url);
+  
+  let fetchData = {
+      method: 'POST',
+      body: aEnvoyer,
+      headers: new Headers()
+    }
+    console.log(fetchData);
+    
+    fetch(url, fetchData)
+.then(function() {
+  // Handle response you get from the server
+});
+   
+
+
+//*******CONTENU DU LS DANS LES CHAMPS DU FORMULAIRE*********/
+// mettre la key du ls dans une variable
+const dataLocalStorage = localStorage.getItem("formulaireValues");
+
+
+//convertir la chaine de caractere en objet JS
+const dataLocalStorageObject = JSON.parse(dataLocalStorage);
+
+//creer une fonction 
+function donneesLs(input) {
+  if(dataLocalStorageObject == null){
+console.log("pas de valeur au local storage");
+  }
+  else{
+      document.querySelector(`#${input}`).value = dataLocalStorageObject[input];  
+  };       
+}
+donneesLs("nom");
+donneesLs("prenom");
+donneesLs("mail");
+donneesLs("numero");
+donneesLs("adresse");
+donneesLs("codepostal");
+donneesLs("ville");
+
+//-------------------------------------------------------------
+
+})//CREATION CLASSE POUR CREER UN OBJET CONTENANT LES VALEURS DU FORMULAIRE
+class Formulaire {
+    constructor() {
+        this.nom = document.querySelector("#nom").value;
+        this.prenom = document.querySelector("#prenom").value;
+        this.mail = document.querySelector("#mail").value;
+        this.numero = document.querySelector("#numero").value;
+        this.adresse = document.querySelector("#adresse").value;
+        this.codepostal = document.querySelector("#codepostal").value;
+        this.ville = document.querySelector("#ville").value;
+    }
+}
+    
+// appel de l 'instance de classe Formulaire pour creer l'objet formulairevalues(recup des valeurs)
+const formulaireValues = new Formulaire();
+
+
+    alerter  (value)  
+        return `${value}:chiffres non autorisés \n caractères compris entre 3 et 20`;
+    
+
+     regExNomPrenomVille(value) 
+        return /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(value);
+    
+
+     controlePrenom() 
+        //controle du prenom
+        const lePrenom = formulaireValues.prenom;
+        if (this.regExNomPrenomVille(lePrenom)) {
+            console.log("ok");
+            return /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(value);;
+        } else {
+            console.log("ko");
+            alert(alerter("Prenom"));
+            return false;
+        }
+    
+
+     controleNom() 
+        //controle du nom
+        const leNom = formulaireValues.nom;
+        if (regExNomPrenomVille(leNom)) {
+            console.log("ok");
+            return true;
+        } else {
+           console.log("ko");
+            alert(alerter("Nom"));
+            return false;
+        }
+    
+
+    regExCodePostal(value)  
+        return /^[0-9]{5}$/.test(value);
+    
+
+     controleCodePostal() 
+        //controle du code postal
+        const leCodePostal = formulaireValues.codepostal;
+        if (regExCodePostal(leCodePostal)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("ko");
+           alert("Le code postal doit etre composé de 5 chiffres");
+            return false;
+        }
+    
+
+     regExEmail (value)  
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
+    
+
+     controleEmail() 
+        //controle de mail
+        const leEmail = formulaireValues.mail;
+        if (regExEmail(leEmail)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("ko");
+           alert(" l'Email n'est pas correct");
+            return false;
+        }
+    
+
+    regExAdresse (value)
+        return /^[A-Za-z0-9\s]{5,50}$/.test(value);
+    
+
+     controleAdresse() 
+        //controle de l'adresse
+        const leAdresse = formulaireValues.adresse;
+        if (regExAdresse(leAdresse)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("ko");
+           alert(" l'adresse n'est pas correcte");
+            return false;
+        }
+      
+
+
+
+
+
+    //controle validité du formulaire avant envoi dans le LS
+    if (controlePrenom() && controleNom() && controleCodePostal() && controleEmail() && controleAdresse()) {
+        //******mettre l'objet formulaireValues dans le localstorage****** */
+        localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
+    } else {
+        alert("Veuillez bien remplir le formulaire");
+    }
+
+    //*******CONTENU DU LS DANS LES CHAMPS DU FORMULAIRE*********/
+// mettre la key du ls dans une variable
+const dataLocalStorage = localStorage.getItem("formulaireValues");
+
+
+//convertir la chaine de caractere en objet JS
+const dataLocalStorageObject = JSON.parse(dataLocalStorage);
+
+//creer une fonction 
+function donneesLs(input) {
+    if(dataLocalStorageObject == null){
+console.log("pas de valeur au local storage");
+    }
+    else{
+        document.querySelector(`#${input}`).value = dataLocalStorageObject[input];  
+    };       
+}
+donneesLs("nom");
+donneesLs("prenom");
+donneesLs("mail");
+donneesLs("numero");
+donneesLs("adresse");
+donneesLs("codepostal");
+donneesLs("ville");
+
+  
+
+
+
+
+
+
+
--- PUBLIC/javascript/panier.js
+++ PUBLIC/javascript/panier.js
@@ -158,8 +158,7 @@ btnEnvoyerFormulaire.addEventListener("click", (e) => {
     e.preventDefault();
     
 
-//******************************************************************************************** */
-//CREATION CLASSE POUR CREER UN OBJET CONTENANT LES VALEURS DU FORMULAIRE
+
 class Formulaire {
     constructor() {
         this.nom = document.querySelector("#nom").value;
@@ -171,30 +170,33 @@ class Formulaire {
         this.ville = document.querySelector("#ville").value;
     }
 }
-// appel de l 'instance de classe Formulaire pour creer l'objet formulairevalues(recup des valeurs)
-const formulaireValues = new Formulaire();
 
+     // appel de l 'instance de classe Formulaire pour creer l'objet formulairevalues(recup des valeurs)
+    const formulaireValues = new Formulaire();
 
-//**************************************GESTION VALIDER FORMULAIRE****************************************************/
-
-const alerter = (value) => {
+  
+  
+  
+  //**************************************GESTION VALIDER FORMULAIRE****************************************************/
+  
+  const alerter = (value) => {
     return `${value}:chiffres non autorisés \n caractères compris entre 3 et 20`;
-}
-const regExNomPrenomVille = (value) => {
+  }
+  const regExNomPrenomVille = (value) => {
     return /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z]{3,20})$/.test(value);
-}
-const regExCodePostal = (value) => {
+  }
+  const regExCodePostal = (value) => {
     return /^[0-9]{5}$/.test(value);
-}
-const regExEmail = (value) => {
+  }
+  const regExEmail = (value) => {
     return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
-}
-
-const regExAdresse = (value) => {
+  }
+  
+  const regExAdresse = (value) => {
     return /^[A-Za-z0-9\s]{5,50}$/.test(value);
-}
-
-function controlePrenom() {
+  }
+  
+  function controlePrenom() {
     //controle du prenom
     const lePrenom = formulaireValues.prenom;
     if (regExNomPrenomVille(lePrenom)) {
@@ -205,9 +207,9 @@ function controlePrenom() {
         alert(alerter("Prenom"));
         return false;
     }
-};
-
-function controleNom() {
+  };
+  
+  function controleNom() {
     //controle du nom
     const leNom = formulaireValues.nom;
     if (regExNomPrenomVille(leNom)) {
@@ -218,9 +220,9 @@ function controleNom() {
         alert(alerter("Nom"));
         return false;
     }
-};
-
-function controleCodePostal() {
+  };
+  
+  function controleCodePostal() {
     //controle du code postal
     const leCodePostal = formulaireValues.codepostal;
     if (regExCodePostal(leCodePostal)) {
@@ -231,9 +233,9 @@ function controleCodePostal() {
        alert("Le code postal doit etre composé de 5 chiffres");
         return false;
     }
-};
-
-function controleEmail() {
+  };
+  
+  function controleEmail() {
     //controle de mail
     const leEmail = formulaireValues.mail;
     if (regExEmail(leEmail)) {
@@ -244,9 +246,9 @@ function controleEmail() {
        alert(" l'Email n'est pas correct");
         return false;
     }
-};
-
-function controleAdresse() {
+  };
+  
+  function controleAdresse() {
     //controle de l'adresse
     const leAdresse = formulaireValues.adresse;
     if (regExAdresse(leAdresse)) {
@@ -257,77 +259,67 @@ function controleAdresse() {
        alert(" l'adresse n'est pas correcte");
         return false;
     }
-};
-    
+  };
+   
     //controle validité du formulaire avant envoi dans le LS
-
+  
     if (controlePrenom() && controleNom() && controleCodePostal() && controleEmail() && controleAdresse()) {
         //******mettre l'objet formulaireValues dans le localstorage****** */
         localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
-        window.location.href = "confirmation.html";
+        //window.location.href = "confirmation.html";
     } else {
         alert("Veuillez bien remplir le formulaire");
     }
-
+  
     //**************************************GESTION VALIDER FORMULAIRE****************************************************/
-
+  
     // creation objet du formulaire et produits a mettre dans un objet pour l'envoyer au serveur
-    const aEnvoyer = {
+    const order = {
         produitLocal,
         formulaireValues,
     };
+    console.log(order);
+  
+  
+    const url = 'http://localhost:3000/api/teddies/order';
+    console.log(url);
     
-envoiServeur(aEnvoyer);
-
-function envoiServeur(aEnvoyer){
-    const essai ={
-        method :"POST",
-        body :JSON.stringify(aEnvoyer),
-        headers:{
-            "Content-Type":"application/json"   
-        }      
-    }
-    fetch('http://localhost:3000/api/teddies/order',essai)
-    .then(res =>res.json())
-    .then(res=>console.log(res));
-    
-     console.log(essai);  
-     console.log(envoiServeur);  
-     }
+    let fetchData = {
+        method: 'POST',
+        body: order,
+        headers: new Headers()
+      }
+      console.log(fetchData);
+      
+      fetch(url, fetchData)
+  .then(function() {
+    // Handle response you get from the server
+  });
      
-
-
-//*******CONTENU DU LS DANS LES CHAMPS DU FORMULAIRE*********/
-// mettre la key du ls dans une variable
-const dataLocalStorage = localStorage.getItem("formulaireValues");
-console.log(dataLocalStorage);
-
-//convertir la chaine de caractere en objet JS
-const dataLocalStorageObject = JSON.parse(dataLocalStorage);
-
-//creer une fonction 
-function donneesLs(input) {
+  
+  
+  //*******CONTENU DU LS DANS LES CHAMPS DU FORMULAIRE*********/
+  // mettre la key du ls dans une variable
+  const dataLocalStorage = localStorage.getItem("formulaireValues");
+  
+  
+  //convertir la chaine de caractere en objet JS
+  const dataLocalStorageObject = JSON.parse(dataLocalStorage);
+  
+  //creer une fonction 
+  function donneesLs(input) {
     if(dataLocalStorageObject == null){
-console.log("pas de valeur au local storage");
+  console.log("pas de valeur au local storage");
     }
     else{
         document.querySelector(`#${input}`).value = dataLocalStorageObject[input];  
     };       
-}
-donneesLs("nom");
-donneesLs("prenom");
-donneesLs("mail");
-donneesLs("numero");
-donneesLs("adresse");
-donneesLs("codepostal");
-donneesLs("ville");
-
-  
-
-
-
-
-
-
-
+  }
+  donneesLs("nom");
+  donneesLs("prenom");
+  donneesLs("mail");
+  donneesLs("numero");
+  donneesLs("adresse");
+  donneesLs("codepostal");
+  donneesLs("ville");
 });
